52030bc9da6008526831f7f1859e96a5703686f5
ozawa <ozawa>
2003-09-26 12:15:23 +0000

    Reduce logging messages.

--- src/org/montsuqi/client/Messages.java
+++ src/org/montsuqi/client/Messages.java
@@ -1,18 +1,8 @@
-/*
- * monsiaj
- * org.montsuqi.client.Messages
- * Copyright (C) 2003 crouton
- *
- * $Id: Messages.java,v 1.2 2003-09-17 05:39:27 ozawa Exp $
- */
 package org.montsuqi.client;
 
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-/**
- * @author crouton
- */
 public class Messages {
 
 	private static final String BUNDLE_NAME = "org.montsuqi.client.messages"; //$NON-NLS-1$
@@ -20,15 +10,9 @@ public class Messages {
 	private static final ResourceBundle RESOURCE_BUNDLE =
 		ResourceBundle.getBundle(BUNDLE_NAME);
 
-	/**
-	 * 
-	 */
 	private Messages() {
 	}
-	/**
-	 * @param key
-	 * @return
-	 */
+
 	public static String getString(String key) {
 		try {
 			return RESOURCE_BUNDLE.getString(key);
--- src/org/montsuqi/monsia/Messages.java
+++ src/org/montsuqi/monsia/Messages.java
@@ -1,16 +1,8 @@
-/*
- * Created on 2003/09/17
- *
- */
 package org.montsuqi.monsia;
 
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-/**
- * @author ozawa
- *
- */
 public class Messages {
 
 	private static final String BUNDLE_NAME = "org.montsuqi.monsia.messages"; //$NON-NLS-1$
@@ -18,15 +10,9 @@ public class Messages {
 	private static final ResourceBundle RESOURCE_BUNDLE =
 		ResourceBundle.getBundle(BUNDLE_NAME);
 
-	/**
-	 * 
-	 */
 	private Messages() {
 	}
-	/**
-	 * @param key
-	 * @return
-	 */
+
 	public static String getString(String key) {
 		try {
 			return RESOURCE_BUNDLE.getString(key);
