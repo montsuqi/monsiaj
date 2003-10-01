b857dd7c0b70d69556b8b1469a2215e84cd78758
ozawa <ozawa>
2003-10-01 06:44:01 +0000

    Avoid runtime cast error.

--- src/org/montsuqi/monsia/Node.java
+++ src/org/montsuqi/monsia/Node.java
@@ -1,19 +1,21 @@
 package org.montsuqi.monsia;
 
+import java.awt.Container;
 import java.util.HashMap;
 import java.util.Map;
-import javax.swing.JWindow;
+import javax.swing.JFrame;
 
 public class Node {
 	public Interface xml;
 	public String name;
-	public JWindow window;
+	public JFrame window;
 	public Map updateWidget;
 
 	public Node(Interface xml, String wName) {
 		this.xml = xml;
 		this.name = wName;
-		this.window = (JWindow)xml.getWidget(wName);
+		Container widget = xml.getWidget(wName);
+		this.window = (JFrame)widget;
 		updateWidget = new HashMap();
 	}
 }
