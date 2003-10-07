148c5de263541059985fe2b076c5c65f1ca8656a
ozawa <ozawa>
2003-10-07 11:42:14 +0000

    setSize stuff also sets preferredSize.

--- src/org/montsuqi/widgets/Fixed.java
+++ src/org/montsuqi/widgets/Fixed.java
@@ -1,6 +1,9 @@
 package org.montsuqi.widgets;
 
+import java.awt.Dimension;
 import java.awt.LayoutManager;
+import java.awt.Rectangle;
+
 import javax.swing.JComponent;
 import org.montsuqi.util.Logger;
 
@@ -11,5 +14,25 @@ public class Fixed extends JComponent {
 	}
 	public void setLayout(LayoutManager layout) {
 		Logger.getLogger(Fixed.class).info(Messages.getString("Fixed.ignoring_Fixed__setLayout")); //$NON-NLS-1$
+	}
+
+	public void setBounds(int x, int y, int width, int height) {
+		super.setBounds(x, y, width, height);
+		setPreferredSize(new Dimension(width, height));
+	}
+
+	public void setBounds(Rectangle r) {
+		super.setBounds(r);
+		setPreferredSize(new Dimension(r.width, r.height));
+	}
+
+	public void setSize(Dimension d) {
+		super.setSize(d);
+		setPreferredSize(d);
+	}
+
+	public void setSize(int width, int height) {
+		super.setSize(width, height);
+		setPreferredSize(new Dimension(width, height));
 	}
 }
