9f6bf48a88f126afaf6234851549fe7996a5edd6
ozawa <ozawa>
2003-12-05 06:07:03 +0000

    toBoolean() accepts uppercase letters.

--- src/org/montsuqi/util/ParameterConverter.java
+++ src/org/montsuqi/util/ParameterConverter.java
@@ -9,7 +9,7 @@ public class ParameterConverter {
 	}
 
 	public static boolean toBoolean(String s) {
-		return "ty".indexOf(s.charAt(0)) >= 0 || toInteger(s) != 0;
+		return "tyTy".indexOf(s.charAt(0)) >= 0 || toInteger(s) != 0;
 	}
 
 	public double toDouble(String s) {
