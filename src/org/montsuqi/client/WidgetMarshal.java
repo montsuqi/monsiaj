d42a7ca3100d3a8402beaabd3fcdfda74991f322
ozawa <ozawa>
2003-12-10 08:20:55 +0000

    add copyright notice

--- src/org/montsuqi/client/WidgetMarshal.java
+++ src/org/montsuqi/client/WidgetMarshal.java
@@ -1,3 +1,25 @@
+/*      PANDA -- a simple transaction monitor
+                                                                                
+Copyright (C) 1998-1999 Ogochan.
+			  2000-2003 Ogochan & JMA (Japan Medical Association).
+                                                                                
+This module is part of PANDA.
+                                                                                
+		PANDA is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY.  No author or distributor accepts responsibility
+to anyone for the consequences of using it or for whether it serves
+any particular purpose or works at all, unless he says so in writing.
+Refer to the GNU General Public License for full details.
+                                                                                
+		Everyone is granted permission to copy, modify and redistribute
+PANDA, but only under the conditions described in the GNU General
+Public License.  A copy of this license is supposed to have been given
+to you along with PANDA so you can know your rights and
+responsibilities.  It should be in a file named COPYING.  Among other
+things, the copyright notice and this notice must be preserved on all
+copies.
+*/
+
 package org.montsuqi.client;
 
 import java.awt.Component;
@@ -54,7 +76,7 @@ class WidgetMarshal {
 		}
 	}
 
-	ValueAttribute getValue(String name) {
+	private ValueAttribute getValue(String name) {
 		return (ValueAttribute)(valueTable.get(name));
 	}
 
@@ -88,6 +110,7 @@ class WidgetMarshal {
 	private void setStyle(Container widget, String style) {
 		logger.debug("apply style \"{0}\" to {1}", new Object[] { style, widget});
 	}
+
 	boolean receiveEntry(Container widget) throws IOException {
 		if (con.receiveDataType()  == Type.RECORD) {
 			int nItem = con.receiveInt();
