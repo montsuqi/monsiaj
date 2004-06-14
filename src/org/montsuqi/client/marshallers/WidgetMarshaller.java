ce82202673ca55529b95d4034ac48a02c35ec2e5
ozawa <ozawa>
2004-06-14 05:45:04 +0000

    remove unused class references

--- src/org/montsuqi/client/marshallers/WidgetMarshaller.java
+++ src/org/montsuqi/client/marshallers/WidgetMarshaller.java
@@ -40,8 +40,6 @@ import javax.swing.JTextField;
 import org.montsuqi.client.Protocol;
 import org.montsuqi.util.Logger;
 import org.montsuqi.widgets.Calendar;
-import org.montsuqi.widgets.CListDummyLabel;
-import org.montsuqi.widgets.NotebookDummyButton;
 import org.montsuqi.widgets.NumberEntry;
 import org.montsuqi.widgets.PandaPreviewPane;
 
@@ -65,10 +63,6 @@ public abstract class WidgetMarshaller {
 		registerMarshaller(Calendar.class, new CalendarMarshaller());
 		registerMarshaller(JProgressBar.class, new ProgressBarMarshaller());
 		registerMarshaller(PandaPreviewPane.class, new PreviewMarshaller());
-
-		// special marshallers for dummy widgets
-		registerMarshaller(CListDummyLabel.class, new DummyLabelMarshaller());
-		registerMarshaller(NotebookDummyButton.class, new DummyButtonMarshaller());
 	}
 
 	WidgetMarshaller() {
