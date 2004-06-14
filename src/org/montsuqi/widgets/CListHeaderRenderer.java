8dea37ffe34b48f5678c09748fdd645b2cc640cf
ozawa <ozawa>
2004-06-14 05:24:02 +0000

    new file

---
+++ src/org/montsuqi/widgets/CListHeaderRenderer.java
@@ -0,0 +1,45 @@
+/*      PANDA -- a simple transaction monitor
+
+Copyright (C) 1998-1999 Ogochan.
+2000-2003 Ogochan & JMA (Japan Medical Association).
+
+This module is part of PANDA.
+
+PANDA is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY.  No author or distributor accepts responsibility
+to anyone for the consequences of using it or for whether it serves
+any particular purpose or works at all, unless he says so in writing.
+Refer to the GNU General Public License for full details.
+
+Everyone is granted permission to copy, modify and redistribute
+PANDA, but only under the conditions described in the GNU General
+Public License.  A copy of this license is supposed to have been given
+to you along with PANDA so you can know your rights and
+responsibilities.  It should be in a file named COPYING.  Among other
+things, the copyright notice and this notice must be preserved on all
+copies.
+*/
+
+package org.montsuqi.widgets;
+
+import java.awt.Component;
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class CListHeaderRenderer implements TableCellRenderer {
+
+	private Component component;
+
+	public CListHeaderRenderer(JComponent c) {
+		c.setBorder(BorderFactory.createEtchedBorder());
+		c.setOpaque(true);
+		c.setForeground(java.awt.Color.red);
+		this.component = c;
+	}
+
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		return component;
+	}
+}
