89cadf2a8a29ddd4e8524318a3b52a5fdc2ddaf1
ozawa <ozawa>
2004-06-29 07:39:28 +0000

    delegete most of Client properties to Configuration

--- src/org/montsuqi/client/Client.java
+++ src/org/montsuqi/client/Client.java
@@ -25,7 +25,6 @@ package org.montsuqi.client;
 import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.text.MessageFormat;
 
 import org.montsuqi.util.Logger;
@@ -33,75 +32,115 @@ import org.montsuqi.util.OptionParser;
 
 public class Client implements Runnable {
 
-	public static final int PORT_GLTERM = 8000;
+	private Configuration conf;
+	private Protocol protocol;
+
 	private static final String CLIENT_VERSION = "0.0"; //$NON-NLS-1$
-	private static final String PANDA_SCHEME = "panda:"; //$NON-NLS-1$
-
-	private int portNumber;
-	private String host;
-	private String cache;
-	private String user;
-	private String pass;
-	private String encoding;
-	private String styles;
-	private String currentApplication;
-
-	// if USE_SSL
-	//private String key;
-	//private String cert;
-	private boolean useSSL;
-	private boolean verify;
-	//private String CApath;
-	//private String CAfile;
-
-	Protocol protocol;
 	private static final Logger logger = Logger.getLogger(Client.class);
-	private int protocolVersion;
 
-	public Client() {
-		protocolVersion = 1;
-	}
-
-	void setConfiguration(Configuration conf) {
-		setUser(conf.getString("user", System.getProperty("user.name"))); //$NON-NLS-1$ //$NON-NLS-2$
-		setPass(new String(conf.getPass()));
-		setHost(conf.getString("host", "localhost")); //$NON-NLS-1$ //$NON-NLS-2$)
-		setPortNumber(conf.getInt("port", Client.PORT_GLTERM)); //$NON-NLS-1$
-		setCurrentApplication(conf.getString("application", "orca00")); //$NON-NLS-1$ //$NON-NLS-2$
-		setEncoding("EUC-JP"); //$NON-NLS-1$
-		setStyles(conf.getString("style", "")); //$NON-NLS-1$ //$NON-NLS-2$
-		setCache(System.getProperty("user.home") + File.separator + "cache"); //$NON-NLS-1$ //$NON-NLS-2$
+	public Client(Configuration conf) {
+		this.conf = conf;
 	}
 
 	private static Client parseCommandLine(String[] args) {
-		Client client = new Client();
+		OptionParser options = new OptionParser();
+		options.add("port", Messages.getString("Client.port_number"), Configuration.DEFAULT_PORT); //$NON-NLS-1$ //$NON-NLS-2$
+		options.add("host", Messages.getString("Client.host_name"), Configuration.DEFAULT_HOST); //$NON-NLS-1$ //$NON-NLS-2$
+		options.add("cache", Messages.getString("Client.cache_directory"), Configuration.DEFAULT_CACHE_PATH); //$NON-NLS-1$ //$NON-NLS-2$
+		options.add("user", Messages.getString("Client.user_name"), Configuration.DEFAULT_USER); //$NON-NLS-1$ //$NON-NLS-2$
+		options.add("pass", Messages.getString("Client.password"), ""); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		options.add("encoding", Messages.getString("Client.server_character_encoding"), Configuration.DEFAULT_ENCODING); //$NON-NLS-1$ //$NON-NLS-2$
+		options.add("style", Messages.getString("Client.styles"), Configuration.DEFAULT_STYLES); //$NON-NLS-1$ //$NON-NLS-2$
+		int protocolVersion = Configuration.DEFAULT_PROTOCOL_VERSION;
+		options.add("v1", Messages.getString("Client.use_protocol_version_1"), protocolVersion == 1); //$NON-NLS-1$ //$NON-NLS-2$
+		options.add("v2", Messages.getString("Client.use_protocol_version_2"), protocolVersion == 2); //$NON-NLS-1$ //$NON-NLS-2$
+		options.add("useSSL", "SSL", Configuration.DEFAULT_USE_SSL); //$NON-NLS-1$ //$NON-NLS-2$
+		options.add("verifypeer", Messages.getString("Client.verify_peer"), Configuration.DEFAULT_VERIFY); //$NON-NLS-1$ //$NON-NLS-2$
 
-		String[] files = client.parseOptions(args);
+		String[] files = options.parse(Client.class.getName(), args);
+		
+		Configuration conf = Configuration.createConfiguration(Client.class); //$NON-NLS-1$
+		conf.setPort(options.getInt("port")); //$NON-NLS-1$
+		conf.setHost(options.getString("host")); //$NON-NLS-1$
+		conf.setCache(options.getString("cache")); //$NON-NLS-1$
+		conf.setUser(options.getString("user")); //$NON-NLS-1$
+		conf.setPass(options.getString("pass")); //$NON-NLS-1$
+		conf.setEncoding(options.getString("encoding")); //$NON-NLS-1$
+		conf.setStyles(options.getString("style")); //$NON-NLS-1$
+
+		boolean v1 = options.getBoolean("v1"); //$NON-NLS-1$
+		boolean v2 = options.getBoolean("v2"); //$NON-NLS-1$
+		if (v1 ^ v2) {
+			if (v1) {
+				conf.setProtocolVersion(1);
+			} else if (v2) {
+				conf.setProtocolVersion(2);
+			} else {
+				assert false : "-v1 or -v2 should have been given."; //$NON-NLS-1$
+			}
+		} else {
+			throw new IllegalArgumentException("specify -v1 or -v2, not both."); //$NON-NLS-1$
+		}
+
+		conf.setUseSSL(options.getBoolean("useSSL")); //$NON-NLS-1$
+		if (conf.getUseSSL()) {
+			//key = options.getString("key");
+			//cert = options.getString("cert");
+			//useSSL = options.getBoolean("ssl");
+			conf.setVerify(options.getBoolean("verifypeer")); //$NON-NLS-1$
+			//CApath = options.getString("CApath");
+			//CAfile = options.getString("CAfile");
+		}
 
 		if (files.length > 0) {
-			client.setCurrentApplication(files[0]);
+			conf.setApplication(files[0]);
 		} else {
-			client.setCurrentApplication(null);
+			conf.setApplication(null);
 		}
-		return client;
+
+		return new Client(conf);
 	}
 
-	public void connect() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnknownHostException, IOException {
-		Socket s = null;
+	public void connect() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+		protocol = new Protocol(this);
+		protocol.sendConnect(conf.getUser(), conf.getPass(), conf.getApplication());
+	}
+
+	Socket createSocket() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 		String factoryName;
 		Object[] options;
-		if (useSSL) {
+		if (conf.getUseSSL()) {
 			factoryName = "org.montsuqi.client.SSLSocketCreator"; //$NON-NLS-1$
-			options = new Object[] { new Boolean(verify) };
+			options = new Object[] { new Boolean(conf.getVerify()) };
 		} else {
 			factoryName = "org.montsuqi.client.SocketCreator"; //$NON-NLS-1$
 			options = null;
 		}
 		Class clazz = Class.forName(factoryName);
 		SocketCreator creator = (SocketCreator)clazz.newInstance();
-		s = creator.create(host, portNumber, options);
-		protocol = new Protocol(this, s, protocolVersion);
-		protocol.sendConnect(user, pass, currentApplication);
+		return creator.create(conf.getHost(), conf.getPort(), options);
+	}
+
+	String getCacheRoot() {
+		StringBuffer cacheRoot = new StringBuffer();
+		cacheRoot.append(conf.getCache());
+		cacheRoot.append(File.separator);
+		cacheRoot.append(conf.getHost());
+		cacheRoot.append(File.separator);
+		cacheRoot.append(conf.getPort());
+		return cacheRoot.toString();
+	}
+
+	String getStyles() {
+		return conf.getStyles();
+	}
+
+	String getEncoding() {
+		return conf.getEncoding();
+	}
+
+	int getProtocolVersion() {
+		return conf.getProtocolVersion();
 	}
 
 	public void run() {
@@ -122,131 +161,6 @@ public class Client implements Runnable {
 			logger.warn(e);
 		} finally {
 			System.exit(0);
-		}
-	}
-
-	private String[] parseOptions(String[] args) {
-		OptionParser options = new OptionParser();
-		String userHome = System.getProperty("user.home"); //$NON-NLS-1$
-		String userName = System.getProperty("user.name"); //$NON-NLS-1$
-
-		options.add("port", Messages.getString("Client.port_number"), PORT_GLTERM); //$NON-NLS-1$ //$NON-NLS-2$
-		options.add("host", Messages.getString("Client.host_name"), "localhost"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		options.add("cache", Messages.getString("Client.cache_directory"), userHome + File.separator + "cache"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		options.add("user", Messages.getString("Client.user_name"), userName); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		options.add("pass", Messages.getString("Client.password"), ""); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		options.add("encoding", Messages.getString("Client.server_character_encoding"), "EUC-JP"); //$NON-LNS-1$ //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		options.add("style", Messages.getString("Client.styles"), ""); //$NON-LNS-1$ //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		options.add("v1", Messages.getString("Client.use_protocol_version_1"), true); //$NON-NLS-1$ //$NON-NLS-2$
-		options.add("v2", Messages.getString("Client.use_protocol_version_2"), false); //$NON-NLS-1$ //$NON-NLS-2$
-		options.add("useSSL", "SSL", false); //$NON-NLS-1$ //$NON-NLS-2$
-		//options.add("key", "key file name(pem)", null);
-		//options.add("cert", "certification file name(pem)", null);
-		//options.add("ssl", "use SSL", false);
-		options.add("verifypeer", Messages.getString("Client.verify_peer"), false); //$NON-NLS-1$ //$NON-NLS-2$
-		//options.add("CApath", "path to CA", null);
-		//options.add("CAfile", "CA file", null);
-
-		String[] files = options.parse(Client.class.getName(), args);
-
-		setPortNumber(options.getInt("port")); //$NON-NLS-1$
-		setHost(options.getString("host")); //$NON-NLS-1$
-		setCache(options.getString("cache")); //$NON-NLS-1$
-		setUser(options.getString("user")); //$NON-NLS-1$
-		setPass(options.getString("pass")); //$NON-NLS-1$
-		setEncoding(options.getString("encoding")); //$NON-NLS-1$
-		setStyles(options.getString("style")); //$NON-NLS-1$
-		setUseSSL(options.getBoolean("useSSL")); //$NON-NLS-1$
-
-		boolean v1 = options.getBoolean("v1"); //$NON-NLS-1$
-		boolean v2 = options.getBoolean("v2"); //$NON-NLS-1$
-
-		if (v1 && ! v2) {
-			setProtocolVersion(1);
-		} else if ( ! v1 && v2) {
-			setProtocolVersion(2);
-		} else {
-			throw new IllegalArgumentException("specify one of -v1 or -v2"); //$NON-NLS-1$
-		}
-		if (useSSL) {
-			//key = options.getString("key");
-			//cert = options.getString("cert");
-			//useSSL = options.getBoolean("ssl");
-			setVerify(options.getBoolean("verifypeer")); //$NON-NLS-1$
-			//CApath = options.getString("CApath");
-			//CAfile = options.getString("CAfile");
-		}
-		return files;
-	}
-
-	private void setProtocolVersion(int version) {
-		protocolVersion = version;
-	}
-
-	public void setPortNumber(int portNumber) {
-		this.portNumber = portNumber;
-	}
-
-	public void setHost(String host) {
-		this.host = host;
-	}
-
-	public void setCache(String cache) {
-		this.cache = cache;
-	}
-
-	String getCacheFileName(String name) {
-		String sep = File.separator;
-		StringBuffer buf = new StringBuffer();
-		buf.append(cache);
-		buf.append(sep);
-		buf.append(host);
-		buf.append(sep);
-		buf.append(portNumber);
-		buf.append(sep);
-		buf.append(name);
-		return buf.toString();
-	}
-
-	public void setUser(String user) {
-		this.user = user;
-	}
-
-	public void setPass(String pass) {
-		this.pass = pass;
-	}
-
-	public void setEncoding(String encoding) {
-		this.encoding = encoding;
-	}
-
-	public String getEncoding() {
-		return encoding;
-	}
-
-	public void setStyles(String styles) {
-		this.styles = styles;
-	}
-
-	public String getStyles() {
-		return styles;
-	}
-
-	public void setUseSSL(boolean useSSL) {
-		this.useSSL = useSSL;
-	}
-
-	public void setVerify(boolean verify) {
-		this.verify = verify;
-	}
-
-	public void setCurrentApplication(String application) {
-		if (application == null) {
-			currentApplication = "demo"; //$NON-NLS-1$
-		} else if ( ! application.startsWith(PANDA_SCHEME)) {
-			currentApplication = PANDA_SCHEME + application;
-		} else {
-			currentApplication = application;
 		}
 	}
 
--- src/org/montsuqi/client/Configuration.java
+++ src/org/montsuqi/client/Configuration.java
@@ -22,45 +22,32 @@ copies.
 
 package org.montsuqi.client;
 
+import java.io.File;
 import java.lang.reflect.Constructor;
 import org.montsuqi.util.Logger;
 
 public abstract class Configuration {
 
-	protected static Logger logger;
-	private String title;
-	private char[] pass;
+	private String pass;
 
-	public Configuration(String title) {
-		this.title = title;
-		logger = Logger.getLogger(Configuration.class);
-	}
+	private static final String PANDA_SCHEME = "panda:"; //$NON-NLS-1$
+	static final int DEFAULT_PORT = 8000;
+	static final String DEFAULT_HOST = "localhost"; //$NON-NLS-1$
+	static final String DEFAULT_USER = System.getProperty("user.anme"); //$NON-NLS-1$
+	static final String DEFAULT_APPLICATION = "demo"; //$NON-NLS-1$
+	static final String DEFAULT_ENCODING = "EUC-JP"; //$NON-NLS-1$
+	static final String DEFAULT_CACHE_PATH = System.getProperty("user.home") + File.separator + "cache";  //$NON-NLS-1$//$NON-NLS-2$
+	static final String DEFAULT_STYLES = ""; //$NON-NLS-1$
+	static final boolean DEFAULT_USE_SSL = false;
+	static final boolean DEFAULT_VERIFY = false;
+	static final int DEFAULT_PROTOCOL_VERSION = 1;
 
-	abstract void save();
+	protected static final Logger logger = Logger.getLogger(Configuration.class);
 
-	public abstract String getString(String key, String defaultValue);
-	public abstract int getInt(String key, int defaultValue);
-
-	public abstract void setString(String key, String value);
-	public abstract void setInt(String key, int value);
-
-	public String getTitle() {
-		return title;
-	}
-
-	public char[] getPass() {
-		return pass;
-	}
-
-	public void setPass(char[] pass) {
-		this.pass = new char[pass.length];
-		System.arraycopy(pass, 0, this.pass, 0, pass.length);
-	}
-
-	public static Configuration createConfiguration(String title, Class clazz) {
+	public static Configuration createConfiguration(Class clazz) {
 		try {
 			Constructor cntr = getConstructor();
-			Object[] params = { title, clazz };
+			Object[] params = { clazz };
 			return (Configuration)cntr.newInstance(params);
 		} catch (Exception e) {
 			logger.fatal(e);
@@ -70,8 +57,8 @@ public abstract class Configuration {
 
 	private static Constructor getConstructor() throws SecurityException, NoSuchMethodException {
 		String[] classes = {
-				"org.montsuqi.client.PreferenceBasedConfiguration", //$NON-NLS-1$
-				"org.montsuqi.client.PropertyFileBasedConfiguration" //$NON-NLS-1$
+			"org.montsuqi.client.PreferenceBasedConfiguration", //$NON-NLS-1$
+			"org.montsuqi.client.PropertyFileBasedConfiguration" //$NON-NLS-1$
 		};
 		Class clazz = null;
 		for (int i = 0; i < classes.length; i++) {
@@ -83,7 +70,119 @@ public abstract class Configuration {
 			}
 		}
 		assert clazz != null;
-		Class[] params = { String.class, Class.class };
+		Class[] params = { Class.class };
 		return clazz.getConstructor(params);
+	}
+
+	abstract void save();
+	abstract String getString(String key, String defaultValue);
+	abstract int getInt(String key, int defaultValue);
+	abstract boolean getBoolean(String key, boolean defaultValue);
+
+	abstract void setString(String key, String value);
+	abstract void setInt(String key, int value);
+	abstract void setBoolean(String key, boolean value);
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public int getPort() {
+		return getInt("port", DEFAULT_PORT); //$NON-NLS-1$
+	}
+
+	public void setPort(int port) {
+		setInt("port", port); //$NON-NLS-1$
+	}
+
+	public String getHost() {
+		return getString("host", DEFAULT_HOST); //$NON-NLS-1$
+	}
+
+	public void setHost(String host) {
+		setString("host", host); //$NON-NLS-1$
+	}
+
+	public String getUser() {
+		return getString("user", DEFAULT_USER); //$NON-NLS-1$
+	}
+
+	public void setUser(String user) {
+		setString("user", user); //$NON-NLS-1$
+	}
+
+	public String getCache() {
+		return getString("cache", DEFAULT_CACHE_PATH); //$NON-NLS-1$
+	}
+
+	public void setCache(String cache) {
+		setString("cache", cache); //$NON-NLS-1$
+	}
+
+	public String getEncoding() {
+		return getString("encoding", DEFAULT_ENCODING); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	public void setEncoding(String encoding) {
+		setString("encoding", encoding); //$NON-NLS-1$
+	}
+
+	public String getStyles() {
+		return getString("styles", DEFAULT_STYLES); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	public void setStyles(String styles) {
+		setString("styles", styles); //$NON-NLS-1$
+	}
+
+	public String getApplication() {
+		return getString("application", DEFAULT_APPLICATION); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	public void setApplication(String app) {
+		if (app == null || app.length() == 0) {
+			app = DEFAULT_APPLICATION; //$NON-NLS-1$
+		} else if ( ! app.startsWith(PANDA_SCHEME)) {
+			app = PANDA_SCHEME + app;
+		}
+		setString("application", app); //$NON-NLS-1$
+	}
+
+	public boolean getUseSSL() {
+		return getBoolean("use_ssl", DEFAULT_USE_SSL); //$NON-NLS-1$
+	}
+
+	public void setUseSSL(boolean flag) {
+		setBoolean("use_ssl", flag); //$NON-NLS-1$
+	}
+
+
+	public boolean getVerify() {
+		return getBoolean("verify", DEFAULT_VERIFY); //$NON-NLS-1$
+	}
+
+	public void setVerify(boolean flag) {
+		setBoolean("verify", flag); //$NON-NLS-1$
+	}
+
+	public int getProtocolVersion() {
+		return getInt("protocol_version", DEFAULT_PROTOCOL_VERSION); //$NON-NLS-1$
+	}
+
+	// look and feel
+	// logger class
+
+	// if USE_SSL
+	//options.add("key", "key file name(pem)", null);
+	//options.add("cert", "certification file name(pem)", null);
+	//options.add("CApath", "path to CA", null);
+	//options.add("CAfile", "CA file", null);
+
+	public void setProtocolVersion(int version) {
+		setInt("protocol_version", version); //$NON-NLS-1$
 	}
 }
