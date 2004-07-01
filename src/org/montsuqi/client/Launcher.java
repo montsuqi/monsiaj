6b11ef953e1c9516cc96b2b13077426a44c65892
ozawa <ozawa>
2004-07-01 06:44:15 +0000

    Introduce logging window by replacing System.out/System.err with those provided by ConsolePane

--- src/org/montsuqi/client/Launcher.java
+++ src/org/montsuqi/client/Launcher.java
@@ -22,9 +22,25 @@ copies.
 
 package org.montsuqi.client;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JOptionPane;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.WindowConstants;
 import org.montsuqi.util.Logger;
+import org.montsuqi.widgets.ConsolePane;
 
 public class Launcher {
 
@@ -36,8 +52,13 @@ public class Launcher {
 
 	static {
 		if (System.getProperty("monsia.logger.factory") == null) { //$NON-NLS-1$
-			System.setProperty("monsia.logger.factory", "org.montsuqi.util.NullLogger"); //$NON-NLS-1$ //$NON-NLS-2$
+			System.setProperty("monsia.logger.factory", "org.montsuqi.util.StdErrLogger"); //$NON-NLS-1$ //$NON-NLS-2$
 		}
+	}
+
+	public static void main(String[] args) {
+		Launcher launcher = new Launcher(Messages.getString("application.title")); //$NON-NLS-1$
+		launcher.launch();
 	}
 
 	public Launcher(String title) {
@@ -50,35 +71,78 @@ public class Launcher {
 	}
 
 	public void launch() {
-		JDialog d = createDialog(conf);
+		JDialog d = createConfigurationDialog(conf);
 		d.setLocationRelativeTo(null);
 		conf.setConfigured(false);
 		d.setVisible(true);
 		if (conf.isConfigured()) {
+			createLogFrame();
 			conf.save();
 			try {
 				target.connect();
 				Thread t = new Thread(target);
 				t.start();
 			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, e.getMessage());
-				System.exit(0);
+				logger.fatal(e);
 			}
 		} else {
 			System.exit(0);
 		}
 	}
 
-	public static void main(String[] args) {
-		Launcher launcher = new Launcher(Messages.getString("application.title")); //$NON-NLS-1$
-		try {
-			launcher.launch();
-		} catch (Exception e) {
-			logger.fatal(e);
-		}
+	public JDialog createConfigurationDialog(Configuration newConf) {
+		return new DefaultConfigurationDialog(title, conf);
 	}
 
-	public JDialog createDialog(Configuration newConf) {
-		return new DefaultConfigurationDialog(title, conf);
+	private void createLogFrame() {
+		final JFrame f = new JFrame(Messages.getString("Launcher.log_title")); //$NON-NLS-1$
+		Container container = f.getContentPane();
+		container.setLayout(new BorderLayout());
+
+		final ConsolePane console = new ConsolePane();
+		System.setOut(console.getOut());
+		System.setErr(console.getErr());
+
+		JScrollPane scroll = new JScrollPane(console);
+		scroll.setPreferredSize(new Dimension(640, 480));
+		container.add(scroll, BorderLayout.CENTER);
+
+		JPanel bar = new JPanel();
+		bar.setLayout(new FlowLayout());
+		container.add(bar, BorderLayout.SOUTH);
+		JButton clear = new JButton(Messages.getString("Launcher.log_clear")); //$NON-NLS-1$
+		clear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				console.setText(""); //$NON-NLS-1$
+			}
+		});
+		bar.add(clear);
+
+		JButton save = new JButton(Messages.getString("Launcher.log_save_log_as")); //$NON-NLS-1$
+		save.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				JFileChooser chooser = new JFileChooser();
+				int ret = chooser.showSaveDialog(f);
+				if (ret == JFileChooser.APPROVE_OPTION) {
+					File file = chooser.getSelectedFile();
+					try {
+						FileWriter fw = new FileWriter(file);
+						fw.write(console.getText());
+						fw.close();
+					} catch (IOException e) {
+						logger.warn(e);
+					}
+				}
+			}
+		});
+		bar.add(save);
+
+		f.setSize(640, 480);
+		int state = f.getExtendedState();
+		f.setExtendedState(state | Frame.ICONIFIED);
+		f.setVisible(true);
+
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 	}
 }
