c059c9fa2e8139fb398e1a824235fab904f61f0e
ozawa <ozawa>
2004-08-04 07:40:01 +0000

    stock image for menu items

--- src/org/montsuqi/widgets/UIStock.java
+++ src/org/montsuqi/widgets/UIStock.java
@@ -38,7 +38,7 @@ public class UIStock {
 		this(text, tooltip, null);
 	}
 
-	private UIStock(String text, String tooltip, String iconPath) {
+	private UIStock(String text, String tooltip, String iconFile) {
 		if (text == null) {
 			throw new NullPointerException("null text"); //$NON-NLS-1$
 		}
@@ -48,7 +48,8 @@ public class UIStock {
 		this.text = text;
 		this.tooltip = tooltip;
 
-		if (iconPath != null) {
+		if (iconFile != null) {
+			String iconPath = "/org/montsuqi/widgets/images/" + iconFile + ".png"; //$NON-NLS-1$ //$NON-NLS-2$
 			URL url = getClass().getResource(iconPath);
 			icon = new ImageIcon(url);
 		}
@@ -75,34 +76,34 @@ public class UIStock {
 
 	static {
 		stocks.put("FILE_TREE",         new UIStock(Messages.getString("UIStock.File"), "")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("NEW_ITEM",          new UIStock(Messages.getString("UIStock.New"), "")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		stocks.put("NEW_ITEM",          new UIStock(Messages.getString("UIStock.New"), "", "new")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		stocks.put("NEW_TREE",          new UIStock(Messages.getString("UIStock.New..."), "")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("OPEN_ITEM",         new UIStock(Messages.getString("UIStock.Open..."), Messages.getString("UIStock.OpenTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("SAVE_ITEM",         new UIStock(Messages.getString("UIStock.Save"), Messages.getString("UIStock.SaveTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("SAVE_AS_ITEM",      new UIStock(Messages.getString("UIStock.SaveAs..."), Messages.getString("UIStock.SaveAsTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("REVERT_ITEM",       new UIStock(Messages.getString("UIStock.Revert"), Messages.getString("UIStock.RevertTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("PRINT_ITEM",        new UIStock(Messages.getString("UIStock.Print"), Messages.getString("UIStock.PrintTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("PRINT_SETUP_ITEM",  new UIStock(Messages.getString("UIStock.PrintSetup..."), Messages.getString("UIStock.PrintSetupTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("CLOSE_ITEM",        new UIStock(Messages.getString("UIStock.Close"), Messages.getString("UIStock.CloseTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("EXIT_ITEM",         new UIStock(Messages.getString("UIStock.Exit"), Messages.getString("UIStock.ExitTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		stocks.put("OPEN_ITEM",         new UIStock(Messages.getString("UIStock.Open..."), Messages.getString("UIStock.OpenTip"), "open")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("SAVE_ITEM",         new UIStock(Messages.getString("UIStock.Save"), Messages.getString("UIStock.SaveTip"), "save")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("SAVE_AS_ITEM",      new UIStock(Messages.getString("UIStock.SaveAs..."), Messages.getString("UIStock.SaveAsTip"), "save_as")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("REVERT_ITEM",       new UIStock(Messages.getString("UIStock.Revert"), Messages.getString("UIStock.RevertTip"), "revert")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("PRINT_ITEM",        new UIStock(Messages.getString("UIStock.Print"), Messages.getString("UIStock.PrintTip"), "print")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("PRINT_SETUP_ITEM",  new UIStock(Messages.getString("UIStock.PrintSetup..."), Messages.getString("UIStock.PrintSetupTip"), "print")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("CLOSE_ITEM",        new UIStock(Messages.getString("UIStock.Close"), Messages.getString("UIStock.CloseTip"), "close")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("EXIT_ITEM",         new UIStock(Messages.getString("UIStock.Exit"), Messages.getString("UIStock.ExitTip"), "exit")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 
 		stocks.put("EDIT_TREE",         new UIStock(Messages.getString("UIStock.Edit"), "")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("CUT_ITEM",          new UIStock(Messages.getString("UIStock.Cut"), Messages.getString("UIStock.CutTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("COPY_ITEM",         new UIStock(Messages.getString("UIStock.Copy"), Messages.getString("UIStock.CopyTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("PASTE_ITEM",        new UIStock(Messages.getString("UIStock.Paste"), Messages.getString("UIStock.PasteTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		stocks.put("CUT_ITEM",          new UIStock(Messages.getString("UIStock.Cut"), Messages.getString("UIStock.CutTip"), "cut")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("COPY_ITEM",         new UIStock(Messages.getString("UIStock.Copy"), Messages.getString("UIStock.CopyTip"), "copy")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("PASTE_ITEM",        new UIStock(Messages.getString("UIStock.Paste"), Messages.getString("UIStock.PasteTip"), "paste")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		stocks.put("SELECT_ALL_ITEM",   new UIStock(Messages.getString("UIStock.SelectAll"), Messages.getString("UIStock.SelectAllTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		stocks.put("CLEAR_ITEM",        new UIStock(Messages.getString("UIStock.Clear"), Messages.getString("UIStock.ClearTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("UNDO_ITEM",         new UIStock(Messages.getString("UIStock.Undo"), Messages.getString("UIStock.UndoTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("REDO_ITEM",         new UIStock(Messages.getString("UIStock.Redo"), Messages.getString("UIStock.RedoTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("FIND_ITEM",         new UIStock(Messages.getString("UIStock.Find..."), Messages.getString("UIStock.FindTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("FIND_AGAIN_ITEM",   new UIStock(Messages.getString("UIStock.FindAgain"), Messages.getString("UIStock.FindAgainTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("REPLACE_ITEM",      new UIStock(Messages.getString("UIStock.Replace..."), Messages.getString("UIStock.ReplaceTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("PROPERTIES_ITEM",   new UIStock(Messages.getString("UIStock.Properties..."), Messages.getString("UIStock.PropertiesTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		stocks.put("UNDO_ITEM",         new UIStock(Messages.getString("UIStock.Undo"), Messages.getString("UIStock.UndoTip"), "undo")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("REDO_ITEM",         new UIStock(Messages.getString("UIStock.Redo"), Messages.getString("UIStock.RedoTip"), "redo")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("FIND_ITEM",         new UIStock(Messages.getString("UIStock.Find..."), Messages.getString("UIStock.FindTip"), "search")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("FIND_AGAIN_ITEM",   new UIStock(Messages.getString("UIStock.FindAgain"), Messages.getString("UIStock.FindAgainTip"), "search")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("REPLACE_ITEM",      new UIStock(Messages.getString("UIStock.Replace..."), Messages.getString("UIStock.ReplaceTip"), "search_replace")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("PROPERTIES_ITEM",   new UIStock(Messages.getString("UIStock.Properties..."), Messages.getString("UIStock.PropertiesTip"), "properties")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 
 		stocks.put("VIEW_TREE",         new UIStock(Messages.getString("UIStock.View"), "")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
 		stocks.put("SETTINGS_TREE",     new UIStock(Messages.getString("UIStock.Settings"), "")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("PREFERENCES_ITEM",  new UIStock(Messages.getString("UIStock.Preerences..."), Messages.getString("UIStock.PreferencesTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		stocks.put("PREFERENCES_ITEM",  new UIStock(Messages.getString("UIStock.Preerences..."), Messages.getString("UIStock.PreferencesTip"), "preferences")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 
 		stocks.put("FILES_TREE",        new UIStock(Messages.getString("UIStock.Files"), "")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
@@ -111,16 +112,16 @@ public class UIStock {
 		stocks.put("CLOSE_WINDOW_ITEM", new UIStock(Messages.getString("UIStock.CloseThisWindow"), Messages.getString("UIStock.CloseThisWindowTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
 		stocks.put("HELP_TREE",         new UIStock(Messages.getString("UIStock.Help"), "")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("ABOUT_ITEM",        new UIStock(Messages.getString("UIStock.About..."), Messages.getString("UIStock.AboutTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		stocks.put("ABOUT_ITEM",        new UIStock(Messages.getString("UIStock.About..."), Messages.getString("UIStock.AboutTip"), "about")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 
 		stocks.put("GAME_TREE",         new UIStock(Messages.getString("UIStock.Game"), "")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		stocks.put("NEW_GAME_ITEM",     new UIStock(Messages.getString("UIStock.NewGame"), Messages.getString("UIStock.NewGameTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("PAUSE_GAME_ITEM",   new UIStock(Messages.getString("UIStock.PauseGame"), Messages.getString("UIStock.PauseGameTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		stocks.put("PAUSE_GAME_ITEM",   new UIStock(Messages.getString("UIStock.PauseGame"), Messages.getString("UIStock.PauseGameTip"), "pause")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		stocks.put("RESTART_GAME_ITEM", new UIStock(Messages.getString("UIStock.RestartGame"), Messages.getString("UIStock.RestartGameTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("UNDO_MOVE_ITEM",    new UIStock(Messages.getString("UIStock.UndoMove"), Messages.getString("UIStock.UndoMoveTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("REDO_MOVE_ITEM",    new UIStock(Messages.getString("UIStock.RedoMove"), Messages.getString("UIStock.RedoMoveTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		stocks.put("UNDO_MOVE_ITEM",    new UIStock(Messages.getString("UIStock.UndoMove"), Messages.getString("UIStock.UndoMoveTip"), "undo")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		stocks.put("REDO_MOVE_ITEM",    new UIStock(Messages.getString("UIStock.RedoMove"), Messages.getString("UIStock.RedoMoveTip"), "redo")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		stocks.put("HINT_ITEM",         new UIStock(Messages.getString("UIStock.Hint"), Messages.getString("UIStock.HintTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		stocks.put("SCORE_ITEM",        new UIStock(Messages.getString("UIStock.Score..."), Messages.getString("UIStock.ScoreTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		stocks.put("SCORE_ITEM",        new UIStock(Messages.getString("UIStock.Score..."), Messages.getString("UIStock.ScoreTip"), "scores")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		stocks.put("END_GAME_ITEM",     new UIStock(Messages.getString("UIStock.EndGame"), Messages.getString("UIStock.EndGameTip"))); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
 }
