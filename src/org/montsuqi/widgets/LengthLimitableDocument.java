a989d160063987adf84115f3d4568efa1f610e98
ozawa <ozawa>
2006-09-21 08:09:04 +0000

    Limit by count of chars, not bytes.

--- src/org/montsuqi/widgets/LengthLimitableDocument.java
+++ src/org/montsuqi/widgets/LengthLimitableDocument.java
@@ -1,7 +1,5 @@
 package org.montsuqi.widgets;
 
-import java.io.UnsupportedEncodingException;
-
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
@@ -27,27 +25,9 @@ public class LengthLimitableDocument extends PlainDocument {
 	}
 
 	public void insertString(int offset, String str, AttributeSet a) throws BadLocationException {
-		int curLength = getLength();
-		String curText = getText(0, curLength);
-		try {
-			final byte[] curBytes = curText.getBytes("EUC-JP");
-			int left = limit - curBytes.length;
-			if (left > 0) {
-				final char[] chars = str.toCharArray();
-				final StringBuffer buf = new StringBuffer();
-				for (int i = 0; i < chars.length; i++) {
-					final String oneChar = String.valueOf(chars[i]);
-					final byte[] bytes = oneChar.getBytes("EUC-JP");
-					if (left < bytes.length) {
-						break;
-					}
-					buf.append(chars[i]);
-					left -= bytes.length;
-	        	}
-				super.insertString(offset, buf.toString(), a);
-	        }
-        } catch (UnsupportedEncodingException e) {
-        	// ignore
-        }
+		final int length = Math.min(str.length(), limit - getLength());
+		if (length > 0) {
+			super.insertString(offset, str.substring(0, length), a);
+		}
     }
 }
