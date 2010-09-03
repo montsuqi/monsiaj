89aef6a81b8b7045dd94e415940cfbbaca38a137
yusuke mihara <mihara@netlab.jp>
2010-09-03 05:14:39 +0000

    * fix PDFPrint can't print correctly with landscape paper

--- src/org/montsuqi/util/PDFPrint.java
+++ src/org/montsuqi/util/PDFPrint.java
@@ -14,7 +14,8 @@ import com.sun.pdfview.PDFFile;
 import com.sun.pdfview.PDFPage;
 
 public class PDFPrint {
-    public static void print(File file,boolean showDialog) throws java.io.IOException {
+
+    public static void print(File file, boolean showDialog) throws java.io.IOException {
         FileInputStream fis = new FileInputStream(file);
         FileChannel fc = fis.getChannel();
         ByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
@@ -23,30 +24,30 @@ public class PDFPrint {
         PDFPrintPage pages = new PDFPrintPage(pdfFile);
 
         PrinterJob pjob = PrinterJob.getPrinterJob();
-        
+
         pjob.setJobName(file.getName());
 
         if (showDialog) {
             if (!pjob.printDialog()) {
-                return ;
+                return;
             }
-         }
-         // validate the page against the chosen printer to correct
-         // paper settings and margins
-         PageFormat pfDefault = pjob.validatePage(pjob.defaultPage());
-         Paper paper = pfDefault.getPaper();
-         paper.setImageableArea(0, 0, paper.getWidth() , paper.getHeight());
-         pfDefault.setPaper(paper);
-         Book book = new Book();
+        }
+        // validate the page against the chosen printer to correct
+        // paper settings and margins
+        PageFormat pfDefault = pjob.validatePage(pjob.defaultPage());
+        Paper paper = pfDefault.getPaper();
+        paper.setImageableArea(0, 0, paper.getWidth(), paper.getHeight());
+        pfDefault.setPaper(paper);
+        Book book = new Book();
 
-         book.append(pages, pfDefault, pdfFile.getNumPages());
-         pjob.setPageable(book);
+        book.append(pages, pfDefault, pdfFile.getNumPages());
+        pjob.setPageable(book);
 
-         try {
-             pjob.print();
-         } catch (PrinterException exc) {
-             System.out.println(exc);
-         }
+        try {
+            pjob.print();
+        } catch (PrinterException exc) {
+            System.out.println(exc);
+        }
     }
 
     public static void main(String args[]) throws Exception {
@@ -56,9 +57,10 @@ public class PDFPrint {
     }
 
     public static class PDFPrintPage implements Printable {
-    
+
         /** The PDFFile to be printed */
-        private PDFFile file;    
+        private PDFFile file;
+
         /**
          * Create a new PDFPrintPage object for a particular PDFFile.
          * @param file the PDFFile to be printed.
@@ -66,28 +68,34 @@ public class PDFPrint {
         public PDFPrintPage(PDFFile file) {
             PDFPrintPage.this.file = file;
         }
-    
+
         // from Printable interface:  prints a single page, given a Graphics
         // to draw into, the page format, and the page number.
         public int print(Graphics g, PageFormat format, int index)
                 throws PrinterException {
             int pagenum = index + 1;
-    
+
             // don't bother if the page number is out of range.
             if ((pagenum >= 1) && (pagenum <= file.getNumPages())) {
-    
+
                 // fit the PDFPage into the printing area
                 Graphics2D g2 = (Graphics2D) g;
                 PDFPage page = file.getPage(pagenum);
-                int width = (int)page.getWidth();
-                int height = (int)page.getHeight();
- 
-                if (width > height) {
-                    g2.transform(new AffineTransform(0f,-1f,1f,0f,0,width));
+                int width = (int) page.getWidth();
+                int height = (int) page.getHeight();
+
+                if (format.getOrientation() == PageFormat.PORTRAIT) {
+                    if (width > height) {
+                        g2.transform(new AffineTransform(0f, -1f, 1f, 0f, 0, width));
+                    }
+                } else if (format.getOrientation() == PageFormat.LANDSCAPE) {
+                    if (height > width) {
+                        g2.transform(new AffineTransform(0f, 1f, -1f, 0f, height, 0));
+                    }
                 }
-    
+
                 // render the page
-                PDFRenderer pgs = new PDFRenderer(page, g2, new Rectangle(0,0,width,height), null, null);
+                PDFRenderer pgs = new PDFRenderer(page, g2, new Rectangle(0, 0, width, height), null, null);
                 try {
                     page.waitForFinish();
                     pgs.run();
