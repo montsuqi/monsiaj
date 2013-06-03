e763030d830466fb377f4a5a58d32dfc43741482
yusuke mihara <mihara@netlab.jp>
2013-06-03 02:57:39 +0000

    * pandaclient.jarの廃止 * monsiaj.jarの追加 * monsiaj-yyyymmdd.zipの追加

---
+++ src/jp/or/med/orca/monsiaj/Loader.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.or.med.orca.monsiaj;
+
+/**
+ *
+ * @author mihara
+ */
+public class Loader {
+        public static void main(String[] args) throws Exception {
+        System.out.println("Loader");
+    }
+}
