cc7a72f595ebf81d519ef997465f64e612560d36
yusuke mihara <mihara@netlab.jp>
2017-06-15 16:08:19 +0900

    add pom.xml

---
+++ src/main/java/jp/or/med/orca/monsiaj/FileUtils.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.jp.or.med.orca.monsiaj;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author mihara
+ */
+public class FileUtils {
+
+    public static void deleteDirectory(File file) throws IOException {
+        if (file.isDirectory()) {
+            for(File f: file.listFiles()) {
+                deleteDirectory(f);
+            }
+            file.delete();
+        } else {
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+}
