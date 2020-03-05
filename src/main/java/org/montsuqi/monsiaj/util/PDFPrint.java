026df415f7a321c27bde67e59d7b0f146e8d2ba4
yusuke mihara <yusuke.mihara@nacl-med.co.jp>
2020-03-05 10:36:15 +0900

    一般的でない用紙サイズの対応

--- src/main/java/org/montsuqi/monsiaj/util/PDFPrint.java
+++ src/main/java/org/montsuqi/monsiaj/util/PDFPrint.java
@@ -1,5 +1,6 @@
 package org.montsuqi.monsiaj.util;
 
+import java.awt.geom.Rectangle2D;
 import java.awt.print.*;
 import java.io.File;
 import java.io.*;
@@ -14,6 +15,7 @@ import javax.print.attribute.PrintRequestAttributeSet;
 import javax.print.attribute.Size2DSyntax;
 import javax.print.attribute.standard.Copies;
 import javax.print.attribute.standard.JobName;
+import javax.print.attribute.standard.MediaPrintableArea;
 import javax.print.attribute.standard.MediaSize;
 import javax.print.attribute.standard.MediaSizeName;
 import org.apache.logging.log4j.LogManager;
@@ -24,19 +26,24 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 import org.montsuqi.monsiaj.client.PrinterConfig;
 
 public class PDFPrint {
+
     private static final Logger LOG = LogManager.getLogger(PDFPrint.class);
     private static final Preferences PREFS = Preferences.userNodeForPackage(PDFPrint.class);
 
     public static void print(File file, int copies, PrintService ps) {
         LOG.debug("print start - " + file);
         try {
-            try (PDDocument document = PDDocument.load(file)) {
-                MediaSizeName size = getMediaSizeName(document);
+            try ( PDDocument doc = PDDocument.load(file)) {
+                MediaSizeName msn = getMediaSizeName(doc);
                 PrinterJob job = PrinterJob.getPrinterJob();
                 job.setPrintService(ps);
-                job.setPageable(new PDFPageable(document));
+                job.setPageable(new PDFPageable(doc));
                 PrintRequestAttributeSet attr = new HashPrintRequestAttributeSet();
-                attr.add(size);
+                if (msn != null) {
+                    attr.add(msn);
+                } else {
+                    attr.add(getMediaPrintableArea(doc));
+                }
                 attr.add(new Copies(copies));
                 attr.add(new JobName(file.getName(), null));
                 PageFormat pf = job.getPageFormat(attr);
@@ -53,19 +60,19 @@ public class PDFPrint {
 
     public static void print(File file) {
         try {
-            try (PDDocument document = PDDocument.load(file)) {
+            try ( PDDocument document = PDDocument.load(file)) {
                 MediaSizeName size = getMediaSizeName(document);
 
                 PrinterJob job = PrinterJob.getPrinterJob();
                 job.setPageable(new PDFPageable(document));
                 PrintService ps = loadPrintService();
                 if (ps != null) {
-                  job.setPrintService(ps);
+                    job.setPrintService(ps);
                 }
                 PrintRequestAttributeSet attr = loadPrintRequestAttributeSet();
                 if (attr == null) {
-                  attr = new HashPrintRequestAttributeSet();
-                  attr.add(size);
+                    attr = new HashPrintRequestAttributeSet();
+                    attr.add(size);
                 }
                 attr.add(new JobName(file.getName(), null));
 
@@ -87,44 +94,44 @@ public class PDFPrint {
     }
 
     public static PrintService loadPrintService() {
-      return PrinterConfig.getPrintService(PREFS.get("printService",""));
+        return PrinterConfig.getPrintService(PREFS.get("printService", ""));
     }
 
     public static void savePrintService(PrintService ps) {
-      PREFS.put("printService",ps.getName());
+        PREFS.put("printService", ps.getName());
     }
 
     public static PrintRequestAttributeSet loadPrintRequestAttributeSet() {
-      try {
-        byte[] array = PREFS.getByteArray("printRequestAttributeSet",new byte[0]);
-        ByteArrayInputStream bais = new ByteArrayInputStream(array);
-        PrintRequestAttributeSet attr;
-          try (ObjectInputStream ois = new ObjectInputStream(bais)) {
-              attr = (PrintRequestAttributeSet)ois.readObject();
-          }
-        return attr;
-      } catch (IOException |ClassNotFoundException ex) {
-        LOG.warn("can not load printRequestAttributeSet");
-        LOG.debug(ex, ex);
-      }
-      return null;
+        try {
+            byte[] array = PREFS.getByteArray("printRequestAttributeSet", new byte[0]);
+            ByteArrayInputStream bais = new ByteArrayInputStream(array);
+            PrintRequestAttributeSet attr;
+            try ( ObjectInputStream ois = new ObjectInputStream(bais)) {
+                attr = (PrintRequestAttributeSet) ois.readObject();
+            }
+            return attr;
+        } catch (IOException | ClassNotFoundException ex) {
+            LOG.warn("can not load printRequestAttributeSet");
+            LOG.debug(ex, ex);
+        }
+        return null;
     }
 
     public static void savePrintRequestAttributeSet(PrintRequestAttributeSet attr) {
-      try {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-          try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
-              oos.writeObject(attr);
-              oos.flush();
-          }
-        PREFS.putByteArray("printRequestAttributeSet",baos.toByteArray());
-      } catch (IOException ex) {
-        LOG.warn(ex, ex);
-      }
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            try ( ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+                oos.writeObject(attr);
+                oos.flush();
+            }
+            PREFS.putByteArray("printRequestAttributeSet", baos.toByteArray());
+        } catch (IOException ex) {
+            LOG.warn(ex, ex);
+        }
     }
 
-    public static MediaSizeName getMediaSizeName(PDDocument document) throws IOException {
-        PDFRenderer renderer = new PDFRenderer(document);
+    public static Rectangle2D.Float getDocumentSize(PDDocument doc) throws IOException {
+        PDFRenderer renderer = new PDFRenderer(doc);
         BufferedImage image = renderer.renderImageWithDPI(0, 72f);
         float w, h, swp;
         w = image.getWidth() / 72f;
@@ -134,7 +141,17 @@ public class PDFPrint {
             w = h;
             h = swp;
         }
-        return MediaSize.findMedia(w, h, Size2DSyntax.INCH);
+        return new Rectangle2D.Float(0, 0, w, h);
+    }
+
+    public static MediaSizeName getMediaSizeName(PDDocument doc) throws IOException {
+        Rectangle2D.Float rect = getDocumentSize(doc);
+        return MediaSize.findMedia(rect.width, rect.height, Size2DSyntax.INCH);
+    }
+
+    public static MediaPrintableArea getMediaPrintableArea(PDDocument doc) throws IOException {
+        Rectangle2D.Float rect = getDocumentSize(doc);
+        return new MediaPrintableArea(0, 0, rect.width, rect.height, Size2DSyntax.INCH);
     }
 
     public static void main(String args[]) throws Exception {
@@ -152,7 +169,7 @@ public class PDFPrint {
             if (ps == null) {
                 PDFPrint.print(new File(args[1]));
             } else {
-                PDFPrint.print(new File(args[1]),1,ps);
+                PDFPrint.print(new File(args[1]), 1, ps);
             }
         }
     }
